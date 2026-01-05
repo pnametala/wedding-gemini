ec55ed3a3297c8ce65ac8681f6c1f970ad9a23cb
Pedro Nametala <pnametala@gmail.com>
2026-01-05 17:52:40 +0800

    push changes

--- src/pages/wedding-raissa.jsx
+++ src/pages/wedding-raissa.jsx
@@ -51,13 +51,13 @@ export async function callGemini(prompt, systemInstruction = "") {
 
 /* --- 5. MAIN APP CONTROLLER --- */
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyC9QRoQ2E0M4qStlDx0rKjW2iJ-MOJQNjA",
+    authDomain: "wedding-nameissa.firebaseapp.com",
+    projectId: "wedding-nameissa",
+    storageBucket: "wedding-nameissa.firebasestorage.app",
+    messagingSenderId: "416682485685",
+    appId: "1:416682485685:web:74af94ea50b2169f4c9bfc",
+    measurementId: "G-93P3D4S60T"
 };
 export const app = initializeApp(firebaseConfig);
 
